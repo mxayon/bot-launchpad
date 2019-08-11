6588086c7a0ca1056c09eec3d6b222afd78fd953
mxayon <mxnkpl@gmail.com>
2019-08-11 12:37:11 -0700

    adds submit for register form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
@@ -12,3 +12,4 @@ class RegisterForm(FlaskForm):
                             validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                             validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
