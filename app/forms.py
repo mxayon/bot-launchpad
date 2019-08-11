ed69e946851b5e7eea2cbf12cd4703d931bdda60
mxayon <mxnkpl@gmail.com>
2019-08-11 12:33:52 -0700

    adds username register function -user,email, password

---
+++ app/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Length, Email
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username',
+                            validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                            validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                            validators=[DataRequired()])
